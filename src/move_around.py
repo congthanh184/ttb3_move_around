8e7a767c3151bf8680a9989aea890057a539863a
congthanh <congthanh184@gmail.com>
2019-07-28 19:57:47 +0800

    Add publish moving forward

--- src/move_around.py
+++ src/move_around.py
@@ -4,11 +4,25 @@ import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 
+pub = nil
+
 def odometry_callback(msg):
-	print msg.pose.pose
+    print msg.pose.pose
+    x = msg.pose.pose.position.x
+    twist = Twist()
+    if x < 0.2:
+        twist.linear.x = 0.1
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+    else:
+        twist.linear.x = 0.0
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+    pub.publish(twist)
 
 if __name__ == '__main__':
     rospy.init_node('move_around', anonymous=True)
     rospy.Subscriber("odom", Odometry, odometry_callback)
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     rospy.spin()
 
