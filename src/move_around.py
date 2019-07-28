184964a5e97315e0ca18d2183083ef423d3bd5a9
congthanh <congthanh184@gmail.com>
2019-07-28 19:34:54 +0800

    Add subscribe odom topic

--- src/move_around.py
+++ src/move_around.py
@@ -1,5 +1,12 @@
 import rospy
-from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 
-def odom_listener():
-	
+def odometry_callback(msg):
+	print msg.pose.pose
+
+if __name__ == '__main__':
+    rospy.init_node('move_around', anonymous=True)
+    rospy.Subscriber("odom", Odometry, odometry_callback)
+	rospy.spin()
+
