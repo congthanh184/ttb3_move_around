1f3c3909a59e8c3897216c54a74fa35973730552
congthanh <congthanh184@gmail.com>
2019-07-28 19:45:37 +0800

    Add missing env python

--- src/move_around.py
+++ src/move_around.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
@@ -8,5 +10,5 @@ def odometry_callback(msg):
 if __name__ == '__main__':
     rospy.init_node('move_around', anonymous=True)
     rospy.Subscriber("odom", Odometry, odometry_callback)
-	rospy.spin()
+    rospy.spin()
 
