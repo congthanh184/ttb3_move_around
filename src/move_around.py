413088f96aeb59e6194908d90fd010541990cfd4
congthanh <congthanh184@gmail.com>
2019-07-28 20:11:27 +0800

    Add reset Odom

--- src/move_around.py
+++ src/move_around.py
@@ -2,7 +2,9 @@
 
 import rospy
 from nav_msgs.msg import Odometry
+from std_msgs.msg import Empty
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
+from time import time
 
 pub = nil
 
@@ -10,8 +12,8 @@ def odometry_callback(msg):
     print msg.pose.pose
     x = msg.pose.pose.position.x
     twist = Twist()
-    if x < 0.2:
-        twist.linear.x = 0.1
+    if x < 0.4:
+        twist.linear.x = 0.05
         twist.linear.y = 0.0
         twist.linear.z = 0.0
     else:
@@ -24,5 +26,12 @@ if __name__ == '__main__':
     rospy.init_node('move_around', anonymous=True)
     rospy.Subscriber("odom", Odometry, odometry_callback)
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    reset_odom = rospy.Publisher('reset', Empty, queue_size=10)
+
+    # reset odometry (these messages take a few iterations to get through)
+    timer = time()
+    while time() - timer < 0.25:
+        reset_odom.publish(Empty())
+
     rospy.spin()
 
